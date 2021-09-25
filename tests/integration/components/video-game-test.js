767ce42ea6299bcf013fa8369840b3b11e165d76
mich <mich-cook@users.noreply.github.com>
2021-09-25 16:21:42 -0700

    add VideoGame component and tests for it

---
+++ tests/integration/components/video-game-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | video-game', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders information about a game', async function (assert) {
+    await render(hbs`<VideoGame />`);
+
+    assert.dom('article').exists();
+    assert.dom('article h3').hasText('Centipede');
+    assert.dom('article .detail.publisher').includesText('Atari');
+    assert.dom('article .detail.release').includesText('1981');
+    assert.dom('article .detail.platform').includesText('Atari 2600');
+    assert.dom('article .detail.multiplayer').includesText('Yes');
+    assert.dom('article .detail.genre').includesText('Shoot');
+  });
+});
