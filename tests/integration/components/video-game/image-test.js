488c608e9c1984a66d8a879e9db243771f6f9031
mich <mich-cook@users.noreply.github.com>
2021-09-26 11:40:53 -0700

    update test file to make eslint happy

--- tests/integration/components/video-game/image-test.js
+++ tests/integration/components/video-game/image-test.js
@@ -7,11 +7,10 @@ module('Integration | Component | video-game/image', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders the given image', async function (assert) {
-
     // can't use these in hbs 5 years after it was reported
     // https://github.com/ember-cli/babel-plugin-htmlbars-inline-precompile/issues/9
-    const src = "/assets/images/Centipede-arcade-flyer.jpg";
-    const alt = "Centipede Arcade Flyer";
+    const src = '/assets/images/Centipede-arcade-flyer.jpg';
+    const alt = 'Centipede Arcade Flyer';
 
     await render(hbs`
       <VideoGame::Image
