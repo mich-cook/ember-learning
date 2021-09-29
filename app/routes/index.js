4fecfd9fc78cb550bda1b333d3518ddeee0a19d0
mich <mich-cook@users.noreply.github.com>
2021-09-29 00:20:09 -0700

    fetch data in route instead of hard coding it there

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,16 +2,11 @@ import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
   async model() {
-    return {
-      title: 'Centipede',
-      publisher: 'Atari',
-      flyerImage: 'https://upload.wikimedia.org/wikipedia/en/6/6b/Centipede-arcade-flyer.jpg',
-      flyerDesc: 'Arcade Flyer',
-      releaseDate: 'June 1981',
-      platform: 'Atari 2600',
-      multiplayer: true,
-      genre: 'Shooter',
-    }
+    const response = await fetch('/api/games.json');
+    const { data } = await response.json();
+    return data.map((model) => {
+      const { attributes } = model;
+      return { ...attributes };
+    });
   }
 }
-
